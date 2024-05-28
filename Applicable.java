01db10be473d61574d9e504bfce73beba651da79
latifaanggia <162555167+latifaanggia@users.noreply.github.com>
2024-05-29 02:54:06 +0700

    Create Applicable.java

---
+++ Applicable.java
@@ -0,0 +1,9 @@
+interface Applicable {
+    String getPromoCode();
+    boolean isCustomerEligible(Customer customer);
+    boolean isMinimumPriceEligible(Order order);
+    boolean isShippingFeeEligible(Order order);
+    double calculateTotalDiscount(Order order) throws Exception;
+    double calculateTotalCashback(Order order) throws Exception;
+    double calculateTotalShippingFeeDiscount(Order order) throws Exception;
+}
