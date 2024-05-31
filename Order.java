6a357410efac36f01a0e20540a9a847bc39ffefa
latifaanggia <162555167+latifaanggia@users.noreply.github.com>
2024-05-31 19:00:23 +0700

    Update Order.java

--- Order.java
+++ Order.java
@@ -1,26 +1,23 @@
 import java.time.LocalDate;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
         LocalDate orderDate;
         LocalDate endDate;
-        int orderNumber;
+        int orderNumber = 1;
         double subTotal;
-        double shippingFee;
-        double discount;
         double total;
         Status status;
         Promotion promotion;
         List<CartItem> menus;
         Customer customer;
 
-        public Order(LocalDate orderDate, LocalDate endDate, double subTotal, double shippingFee, double discount, double total, Customer customer) {
+        public Order(LocalDate orderDate, LocalDate endDate, double subTotal, double total, Customer customer) {
                 this.orderDate = orderDate;
                 this.endDate = endDate;
-//                this.orderNumber = orderNumber;
                 this.subTotal = subTotal;
-                this.shippingFee = shippingFee;
-                this.discount = discount;
-                this.total += subTotal + shippingFee - discount;
+                this.total += subTotal;
                 this.customer = customer;
                 this.menus = new ArrayList<>();
                 this.status = Status.UNPAID;
@@ -30,23 +27,7 @@ public class Order {
         public List<CartItem> getMenus() {
                 return menus;
         }
-
-        // class Pelanggan harus diisi dari constructor
-        public void checkOut() {
-                if (status == Status.UNPAID) {
-                        status = Status.SUCCESSFUL;
-                        this.orderDate = LocalDate.now();
-                } else {
-                        System.out.println("The order has been paid or cancelled.");
-                }
-        }
-
-        public void pay() {
-                if (status == Status.SUCCESSFUL) {
-                        System.out.println("Payment successful.");
-                        status = Status.SUCCESSFUL;
-                } else {
-                        System.out.println("Order has not been paid for.");
-                }
+        public void setStatus(Status status) {
+                this.status = status;
         }
 }
