e99aeb19c95d10ccfe27157b131e68ceba06ac3c
latifaanggia <162555167+latifaanggia@users.noreply.github.com>
2024-05-31 19:01:18 +0700

    Update Applicable.java

--- Applicable.java
+++ Applicable.java
@@ -4,5 +4,4 @@ interface Applicable {
     boolean isMinimumPriceEligible(Order order);
     double calculateTotalDiscount(Order order) throws Exception;
     double calculateTotalCashback(Order order) throws Exception;
-    double calculateTotalShippingFeeDiscount(Order order) throws Exception;
 }
